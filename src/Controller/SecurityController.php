97bd6ce91e30869f83a9ca1a06b87e6d3a994380
yaminabes <nanacosmetic90@gmail.com>
2021-11-10 11:48:36 +0100

    inscription avec influence bdd

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,14 +17,16 @@ class SecurityController extends AbstractController
      * @Route("/inscription", name="security_registration")
      */
 
-    public function registration(Request $request, ManagerRegistry $manager){
+    public function registration(Request $request){
         $user = new User();
-
+        $manager = $this->getDoctrine()->getManager();
         $form = $this->createForm(RegistrationType::class, $user);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
+            $user->setAdresse("adresse");
+            $user->setNumTel("094238");
             $manager->persist($user);
             $manager->flush();
         }
