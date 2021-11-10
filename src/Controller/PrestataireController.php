b5108d65e63c8c5d89c3980830ebd0b56884a911
yaminabes <nanacosmetic90@gmail.com>
2021-11-10 09:03:21 +0100

    adding templates

---
+++ src/Controller/PrestataireController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Prestataire;
+use App\Form\PrestataireType;
+use App\Repository\PrestataireRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/prestataire')]
+class PrestataireController extends AbstractController
+{
+    #[Route('/', name: 'prestataire_index', methods: ['GET'])]
+    public function index(PrestataireRepository $prestataireRepository): Response
+    {
+        return $this->render('prestataire/index.html.twig', [
+            'prestataires' => $prestataireRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'prestataire_new', methods: ['GET','POST'])]
+    public function new(Request $request): Response
+    {
+        $prestataire = new Prestataire();
+        $form = $this->createForm(PrestataireType::class, $prestataire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($prestataire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('prestataire_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('prestataire/new.html.twig', [
+            'prestataire' => $prestataire,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'prestataire_show', methods: ['GET'])]
+    public function show(Prestataire $prestataire): Response
+    {
+        return $this->render('prestataire/show.html.twig', [
+            'prestataire' => $prestataire,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'prestataire_edit', methods: ['GET','POST'])]
+    public function edit(Request $request, Prestataire $prestataire): Response
+    {
+        $form = $this->createForm(PrestataireType::class, $prestataire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('prestataire_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('prestataire/edit.html.twig', [
+            'prestataire' => $prestataire,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'prestataire_delete', methods: ['POST'])]
+    public function delete(Request $request, Prestataire $prestataire): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$prestataire->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($prestataire);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('prestataire_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/PrestataireType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Metier;
+use App\Entity\Prestataire;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PrestataireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('metiers', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Metier::class,
+
+                // uses the User.username property as the visible option string
+                'choice_label' => 'corps_metier',
+
+                // used to render a select box, check boxes or radios
+                 'multiple' => true,
+                 'expanded' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Prestataire::class,
+        ]);
+    }
+}
