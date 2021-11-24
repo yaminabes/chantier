29bbb5a233b344359c9198cd5665b9d154ca852e
BokumunOglu <furkan.sertdemir70000@gmail.com>
2021-11-24 12:14:52 +0100

    User ok

--- assets/prestataireFill.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function (){
-    $('#tache_metier').change(function (){
-        let id_metier = $('#tache_metier').val()
-    })
-    //let prestataires = {{prestataires}}
-    console.log(prestataires)
-
-})
